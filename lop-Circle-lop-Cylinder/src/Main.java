3cc7c1e3f55c4289ca70f2be823b5646b2966db2
Nguyen Hung <billy7d95@gmail.com>
2020-08-03 10:57:26 +0700

    add

---
+++ lop-Circle-lop-Cylinder/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Cylinder cylinder = new Cylinder(10.0,5.0,"yellow");
+        System.out.println(cylinder.getVolume());
+    }
+}
