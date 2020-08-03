8f2c129eee049b5bec3895ef84e275c5ae594f22
Nguyen Hung <billy7d95@gmail.com>
2020-08-03 14:23:34 +0700

    movable point

---
+++ Week 2/point-moveAblePoint/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Point point = new Point(10,10);
+        System.out.println(point.toString());
+
+        MovablePoint mp = new MovablePoint(1,2,3,4);
+        System.out.println(mp.toString());
+    }
+
+
+
+}
