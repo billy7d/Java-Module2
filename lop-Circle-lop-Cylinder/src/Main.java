4188ce209e534310370725e02633d8833196fe2f
Nguyen Hung <billy7d95@gmail.com>
2020-08-04 22:17:26 +0700

    xoa

--- lop-Circle-lop-Cylinder/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Cylinder cylinder = new Cylinder(10.0,5.0,"yellow");
-        System.out.println(cylinder.getVolume());
-    }
-}
