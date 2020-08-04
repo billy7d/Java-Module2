6c7f081c03bdabe50d9dcd370b78a12487f3162b
Nguyen Hung <billy7d95@gmail.com>
2020-08-04 14:12:31 +0700

    s

--- Week 2/da-hinh/src/Animal.java
+++ Week 2/da-hinh/src/Animal.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class Animal {
+
+    public double height = 10;
+    public double width = 10;
+
+    public Animal(){
+
+    }
+
+    public Animal(double height,double width){
+        this.height = height;
+        this.width = width;
+    }
+
+    public void sound(){
+        System.out.println("Gau gau");
+    }
+
+
 }
--- Week 2/da-hinh/src/Cat.java
+++ Week 2/da-hinh/src/Cat.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+public class Cat extends Animal {
+    public Cat(){}
 
-public class Cat {
+    public Cat(double height,double width){
+        super(height, width);
+    }
+
+    @Override
+    public void sound() {
+
+        System.out.println("Meo meo");
+    }
 }
