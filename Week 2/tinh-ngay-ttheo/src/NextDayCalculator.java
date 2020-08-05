ff2b91b462b35ded21897ee2661cd95e28c64375
Nguyen Hung <billy7d95@gmail.com>
2020-08-05 17:21:58 +0700

    next day calculator

--- Week 2/tinh-ngay-ttheo/src/NextDayCalculator.java
+++ Week 2/tinh-ngay-ttheo/src/NextDayCalculator.java
@@ -1,7 +1,51 @@
-public class NextDayCalculator {
-    public static int printNextDay(int day){
-        throw new UnsupportedOperationException();
+import java.util.Scanner;
 
+public class NextDayCalculator {
+    public static int getDaysOfMonth(int month) {
+        switch (month){
+            case 1,3,5,7,8,10,12:
+                    return  31;
+
+            case 4,6,9,11:
+                 return 30;
+
+            case 2:
+                //bo sung leapyear
+                break;
+            default:
+                System.out.println("Nhap sai");
+                break;
+
+        } return getDaysOfMonth(month);
 
     }
+
+    public static int getNextDay(int day,int month){
+        if (day == getDaysOfMonth(month)){
+            day = 1;
+        } else if (day<getDaysOfMonth(month)){
+            day +=1;
+        }
+        return day;
+    }
+
+    public static int getMonth(int day, int month) {
+            if (day == getDaysOfMonth(month) && month < 12) {
+            month += 1;
+            } else if (day == getDaysOfMonth(month) && month ==12){
+            month =1;
+        }
+            return month;
+    }
+
+    public static int getYear(int day,int month,int year) {
+        if(getDaysOfMonth(month) ==31 && month ==12){
+            year+=1;
+        } return year;
+    }
+
+    public static String getDayMonthYear(int day,int month, int year){
+        return ""+getNextDay(day,month) +"/" + getMonth(day, month)+"/"+ getYear(day, month, year);
+    }
 }
+
--- Week 2/tinh-ngay-ttheo/test/NextDayCalculatorTest.java
+++ Week 2/tinh-ngay-ttheo/test/NextDayCalculatorTest.java
@@ -1,13 +1,58 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class NextDayCalculatorTest {
 
-    @org.junit.jupiter.api.Test
-    void testPrintNextDay() {
-        int day =1;
-        int month =1;
-        int year = 2018;
+    @Test
+    void testGetDaysOfMonth() {
+        int day =30;
+        int month =4;
+        int expected = 7;
+
+        int result = NextDayCalculator.getDaysOfMonth(month);
+        assertEquals(expected,result);
+
+    }
+    @Test
+    void testGetNextDay(){
+        int day =23;
+        int month =5;
+        int expected = 23;
+
+        int result1 = NextDayCalculator.getNextDay(day,month);
+        assertEquals(expected,result1);
     }
 
+    @Test
+    void testGetMonth(){
+        int day =30;
+        int month =12;
+        int expected = 5;
 
+        int result2 = NextDayCalculator.getMonth(day,month);
+        assertEquals(expected,result2);
+    }
+
+    @Test
+    void testGetYear(){
+        int day =31;
+        int month =12;
+        int year =2020;
+        int expected = 2021;
+
+        int result3 = NextDayCalculator.getYear(day,month,year);
+        assertEquals(expected,result3);
+    }
+
+    @Test
+    void testGetDayMonthYear(){
+        int day =25;
+        int month =4;
+        int year =2020;
+        int expected = 31122021;
+
+        String result4 = NextDayCalculator.getDayMonthYear(day,month,year);
+        assertEquals(expected,result4);
+    }
 }
