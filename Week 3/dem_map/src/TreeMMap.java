41bfc214ab81d1aa4a2e5449b31e259b0beacd57
Nguyen Hung <billy7d95@gmail.com>
2020-08-10 21:09:54 +0700

    chua xong

--- Week 3/dem_map/src/TreeMMap.java
+++ Week 3/dem_map/src/TreeMMap.java
@@ -4,13 +4,22 @@ import java.util.TreeMap;
 public class TreeMMap {
     public static void main(String[] args) {
         TreeMap<String,Integer> map = new TreeMap<>();
-        String str = "Abc con be con bo";
+        String str = "abc con ga be bo con con cho";
         String[] split = str.split(" ");
-        int key =1;
+
+
+        int value =1;
 
         for (int i = 0; i < split.length; i++) {
-            map.put(split[i],key);
+                if (map.containsKey(split[i])) {
+                    value++;
+
+                } else {
+                    map.put(split[i], value);
+                }
         }
+
+
 
         System.out.println(map);
 
