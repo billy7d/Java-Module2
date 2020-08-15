4c376f5c2cdefa90fd3ddc779f90a41ae2949ce4
Nguyen Hung <billy7d95@gmail.com>
2020-08-15 23:55:54 +0700

    add

--- Week 4/Lesson 17/btap_qlysanpham/src/Main.java
+++ Week 4/Lesson 17/btap_qlysanpham/src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         Product product = new Product(0, "Iphone", "Apple", 1000, null);
         Product product1 = new Product(1, "Galaxy", "Samsung", 900, null);
         Product product2 = new Product(2, "Mate20", "Huawei", 950, null);
-        Product product3 = new Product(3, "Bphone pro", "Bphone", 800, null);
+        Product product3 = new Product(3, "Bphone1", "Bphone", 800, null);
 
 
         productManager.addProduct(arrayList, product);
@@ -29,7 +29,7 @@ public class Main {
         }
 
 
-//        System.out.println(arrayList.get(2));
+
     }
 }
 
