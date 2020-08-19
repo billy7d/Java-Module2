cc68d1f6a8743aeb5f4dc18c245003238397a71c
Nguyen Hung <billy7d95@gmail.com>
2020-08-19 23:21:00 +0700

    add

--- Week 4/Lesson 18/untitled1/src/FactoryPatternDemo.java
+++
@@ -1,9 +0,0 @@
-public class FactoryPatternDemo {
-    public static void main(String[] args) {
-        ShapeFactory shapeFactory = new ShapeFactory();
-        Shape a = shapeFactory.getShape("Circle");
-        a.draw();
-
-
-    }
-}
--- Week 4/Lesson 18/untitled1/src/Shape.java
+++
@@ -1,5 +0,0 @@
-public interface Shape {
-    default void draw(){
-
-    }
-}
