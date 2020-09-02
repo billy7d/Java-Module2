71e539c51d4e8f5dbbbbc0e28ea6dd59c397ab56
Nguyen Hung <billy7d95@gmail.com>
2020-09-02 17:54:33 +0700

    add

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Test {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] x = {5, 6, 7, 8};
+
+        for (int a : x) {
+
+            if (a % 2 == 0) {
+
+                break;
+
+            }
+
+            System.out.print(a);
+
+        }
+    }
 }
